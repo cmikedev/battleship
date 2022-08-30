c267e83bc424e039922f20bb8a65b2b6fdd69ba8
cmikedev <carberrm@gmail.com>
2022-08-30 11:54:53 +0000

    Correction: start_message function unsaved in last commit

--- run.py
+++ run.py
@@ -32,25 +32,28 @@ def create_board(board):
     for row in board:
         print((" ").join(row))
 
-
-print("Welcome to Battleship")
-sleep(3)
-os.system("clear")
-print("Warning!!!! \nEnemy forces have invaded our waters!")
-sleep(3)
-print("\nThe enemy forces are equipped with the latest \ncloaking technology making them invisible to our \nradars. Luckily our gunnar engineers are able to \ndraw up grid maps on the fly to assist us \nin aiming our shells.")
-sleep(3)
-print("\nAs the gunnar who has won on more scratch cards than any \nother you have been chose to fire blindly into the sea. \n Congratulations!")
-sleep(5)
-os.system("clear")
-print("Here is what you must do to defeat the invaders:")
-sleep(3)
-print("1. Select the grid ROW which will appear as a NUMBER")
-sleep(3)
-print("2. Select the grid COLUMN which will appear as a LETTER")
-sleep(3)
-print("Are you ready?")
-input("Press any key to continue....")
-if input != " ":
-  os.system('clear')
-print("let's go!!!")
+def start_message():
+    """
+    This function welcomes the player and provides instructions on how to play.
+    """
+    print("Welcome to Battleship")
+    sleep(3)
+    os.system("clear")
+    print("Warning!!!! \nEnemy forces have invaded our waters!")
+    sleep(3)
+    print("\nThe enemy forces are equipped with the latest \ncloaking technology making them invisible to our \nradars. Luckily our gunnar engineers are able to \ndraw up grid maps on the fly to assist us \nin aiming our shells.")
+    sleep(3)
+    print("\nAs the gunnar who has won on more scratch cards than any \nother you have been chose to fire blindly into the sea. \n Congratulations!")
+    sleep(5)
+    os.system("clear")
+    print("Here is what you must do to defeat the invaders:")
+    sleep(3)
+    print("1. Select the grid ROW which will appear as a NUMBER")
+    sleep(3)
+    print("2. Select the grid COLUMN which will appear as a LETTER")
+    sleep(3)
+    print("Are you ready?")
+    input("Press any key to continue....")
+    if input != " ":
+    os.system('clear')
+    print("let's go!!!")
