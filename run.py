2341acaab72131278ebcae7a4575a9b4158d6bf7
cmikedev <carberrm@gmail.com>
2022-08-30 11:52:02 +0000

    Add imports and start_message function which provides player with instructions on how to play

--- run.py
+++ run.py
@@ -2,8 +2,10 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import os
+from time import sleep
 
-difficulty_level = {"a": [6, 20], "b": [8, 36], "c": [10, 50]}
+difficulty_level = {"a": [6, 20], "b": [8, 36], "c": [10, 45]}
 board = []
 
 
@@ -30,4 +32,25 @@ def create_board(board):
     for row in board:
         print((" ").join(row))
 
-print(create_board(board))
+
+print("Welcome to Battleship")
+sleep(3)
+os.system("clear")
+print("Warning!!!! \nEnemy forces have invaded our waters!")
+sleep(3)
+print("\nThe enemy forces are equipped with the latest \ncloaking technology making them invisible to our \nradars. Luckily our gunnar engineers are able to \ndraw up grid maps on the fly to assist us \nin aiming our shells.")
+sleep(3)
+print("\nAs the gunnar who has won on more scratch cards than any \nother you have been chose to fire blindly into the sea. \n Congratulations!")
+sleep(5)
+os.system("clear")
+print("Here is what you must do to defeat the invaders:")
+sleep(3)
+print("1. Select the grid ROW which will appear as a NUMBER")
+sleep(3)
+print("2. Select the grid COLUMN which will appear as a LETTER")
+sleep(3)
+print("Are you ready?")
+input("Press any key to continue....")
+if input != " ":
+  os.system('clear')
+print("let's go!!!")
