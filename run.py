b8744ef2d65bf6c86ce471635329f047d8f5015b
cmikedev <carberrm@gmail.com>
2022-08-30 14:16:28 +0000

    Update start_message and import random

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 
 import os
 from time import sleep
+import random
 
 difficulty_level = {"a": [6, 20], "b": [8, 36], "c": [10, 45]}
 board = []
@@ -32,28 +33,60 @@ def create_board(board):
     for row in board:
         print((" ").join(row))
 
+def create(ships):
+    """
+    This function creates the invading ship dimensions
+    """
+    pass
+
 def start_message():
     """
     This function welcomes the player and provides instructions on how to play.
     """
-    print("Welcome to Battleship")
+
+    def continue_key():
+        input("Press any key to continue....")
+        if input != " ":
+            os.system('clear')
+
+    intro = [
+        "Warning!!!",
+        "\nEnemy forces have invaded our waters!!!\n",
+        "\nThe enemy forces are equipped with the latest cloaking technology making them invisible to our radars.\n",
+        "\nLuckily our gunnar engineers are able to draw up grid maps on the fly to assist us in aiming our shells.\n",
+        "\nAs the gunnar who has won on more scratch cards than any other you have been chosen to fire blindly into the sea and hopefully destroy the enemy fleet.\n",
+        "\nCongratulations!\n"
+    ]
+    
+    instructions = [
+        "\nYou must input two coordinates, a number and a letter in order to select the grid you want to fire upon\n",
+        "\n1. Select the grid ROW which will appear as a NUMBER\n",
+        "\n2. Select the grid COLUMN which will appear as a LETTER\n",
+        "\nIf a ship is within the grid coordinates you selected, a hit will be registered\n",
+        "\nIf the grid is empty, it will be registered as a miss",
+        "\nThe size of the grid and the amount of shells you have will be determined by the difficulty level you choose\n"
+    ]
+    
+    print("BATTLESHIP")
     sleep(3)
     os.system("clear")
-    print("Warning!!!! \nEnemy forces have invaded our waters!")
-    sleep(3)
-    print("\nThe enemy forces are equipped with the latest \ncloaking technology making them invisible to our \nradars. Luckily our gunnar engineers are able to \ndraw up grid maps on the fly to assist us \nin aiming our shells.")
-    sleep(3)
-    print("\nAs the gunnar who has won on more scratch cards than any \nother you have been chose to fire blindly into the sea. \n Congratulations!")
-    sleep(5)
-    os.system("clear")
+
+    for i in intro:
+        print(i)
+        sleep(3)
+
+    continue_key()
+        
     print("Here is what you must do to defeat the invaders:")
     sleep(3)
-    print("1. Select the grid ROW which will appear as a NUMBER")
-    sleep(3)
-    print("2. Select the grid COLUMN which will appear as a LETTER")
-    sleep(3)
+    
+    for j in instructions:
+        print(j)
+        sleep(3)
+
+
     print("Are you ready?")
-    input("Press any key to continue....")
-    if input != " ":
-    os.system('clear')
+    continue_key()
     print("let's go!!!")
+
+start_message()
